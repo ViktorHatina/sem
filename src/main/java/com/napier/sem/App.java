71ebec3a2f53b411b52b955a6d7d8fa90710a200
ViktorHatina <hatina.viktor@gmail.com>
2022-03-24 00:55:10 +0100

    Enabling Local Debugging

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -325,14 +325,16 @@ public class App {
 
         // Connect to database
         if (args.length < 1) {
-            a.connect("localhost:3306", 30000);
+            a.connect("localhost:33060", 0);
         } else {
-            a.connect(args[0], Integer.parseInt(args[1]));
+            a.connect("db:3306", 30000);
         }
 
+
         //Print all salaries by departmen
-        ArrayList<Employee> employees = a.getSalariesByDepartment("Sales");
-        //ArrayList<Employee> employees = a.getSalariesByTitle("Engineer");
+        //ArrayList<Employee> employees = a.getSalariesByDepartment("Sales");
+        ArrayList<Employee> employees = a.getSalariesByTitle("Engineer");
+        //ArrayList<Employee> employees = a.getSalariesByDepartment("Development");
         a.printSalaries(employees);
 
         a.disconnect();
